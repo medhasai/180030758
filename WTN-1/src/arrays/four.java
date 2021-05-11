d88dde026a412d0d825f0ad6db7ac0b8e837bd0f
medhu <medhasai25@gmail.com>
2021-05-11 12:13:25 +0530

    Arrays

---
+++ WTN-1/src/arrays/four.java
@@ -0,0 +1,18 @@
+package arrays;
+import java.util.Scanner;
+public class four {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		char ch=0;
+		int i;
+		for(i=0;i<n;i++) {
+			ch=(char)a[i];
+			System.out.print(ch);
+		}
+		sc.close();
+	}
+}
