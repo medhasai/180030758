10760f5781d988c7faf97a03568b75bc69ff8d15
medhasai <56549422+medhasai@users.noreply.github.com>
2021-06-11 11:29:15 +0530

    Add files via upload

---
+++ WTN_TM01/InvalidConnectionException.java
@@ -0,0 +1,13 @@
+//exception
+package WTN_TM01;
+public class InvalidConnectionException extends Exception{
+public String toString()
+{
+return "Invalid ConnectionType";
+}
+public InvalidConnectionException()
+{
+super();
+}
+}
+
---
+++ WTN_TM01/InvalidReadingException.java
@@ -0,0 +1,12 @@
+//exception
+package WTN_TM01;
+public class InvalidReadingException extends Exception{
+public String toString()
+{
+return "Incorrect Reading";
+}
+public InvalidReadingException()
+{
+super();
+}
+}
